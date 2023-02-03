5ce7060b1c2552e1417d9ca4097bdb09b20569ba
burjepaul <burjepaul97@gmail.com>
2023-02-03 18:10:08 +0200

    Created the Burger Menu

--- src/components/category-item/category-item.component.jsx
+++ src/components/category-item/category-item.component.jsx
@@ -5,13 +5,13 @@ const CategoryItem = ({category}) => {
     const {title, imageUrl, path} = category
     return (
         <Link className='category-container' to={path}>
-                <div 
-                    className='background-image'
-                    style= {{
-                        backgroundImage: 'url(' + require(`../../assets/${imageUrl}.png`) + ')'
-                    }}    
-                    />
-                <div className='category-body-container'>
+                    <div 
+                        className='background-image'
+                        style= {{
+                            backgroundImage: 'url(' + require(`../../assets/${imageUrl}.png`) + ')'
+                        }}    
+                        />
+                    <div className='category-body-container'>
                     <h2>{title}</h2>
                 </div>
          </Link>
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,35 +1,92 @@
-import { Fragment } from "react"
+import { Fragment, useState } from "react"
 import { Outlet, Link } from "react-router-dom"
 
 import {ReactComponent as Logo} from '../../assets/logo.svg'
 
 import './navigation.styles.scss'
+import './burger-menu.styles.scss'
 
 const Navigation = () => {
+    const [burger_class, setBurgerClass] = useState("burger-bar unclicked")
+    const [menu_class, setMenuClass] = useState("menu hidden")
+    const [isMenuClicked, setIsMenuClicked] = useState(false)
+    
+    const updateMenu = () => {
+        if(!isMenuClicked){
+            setBurgerClass("burger-bar clicked")
+            setMenuClass("menu visible")
+        }
+        else{
+            setBurgerClass("burger-bar unclicked")
+            setMenuClass("menu hidden")
+        }
+        setIsMenuClicked(!isMenuClicked)
+    }
+    
+    const viewportWidth = window.innerWidth;
+
+    if (viewportWidth < 500 ) {
     return (
-      <Fragment>
-        <div className="navigation">
-            <Link className="logo-container" to="/">
-                <Logo className="logo"/>
-            </Link>
-            <div className="nav-links-container">
-                {/* <Link className="nav-link" to='/shop'>
-                    About us
-                </Link> */}
-                <Link className="nav-link" to='/shop'>
-                    Forum
+        <Fragment>
+            <div style={{width:'100%', height:'9vh'}}>
+                <Link className="logo-container" to="/">
+                    <Logo className="logo"/>
                 </Link>
-                <Link className="nav-link" to='/shop'>
-                    Contact
-                </Link>
-                <Link className="nav-link" to='/shop'>
-                    Sign In
-                </Link>
+                <nav>
+                    <div className="burger-menu" onClick={updateMenu}>
+                        <div className={burger_class}></div>
+                        <div className={burger_class}></div>
+                        <div className={burger_class}></div>
+                    </div>
+                </nav>
+
+                <div className={menu_class}>
+                        <Link className="nav-link-mini" to='/shop'>
+                            About us
+                        </Link>
+                        <Link className="nav-link-mini" to='/shop'>
+                            Forum
+                        </Link>
+                        <Link className="nav-link-mini" to='/shop'>
+                            Contact
+                        </Link>
+                        <Link className="nav-link-mini" to='/shop'>
+                            Sign In
+                        </Link>
+                </div>
+            <Outlet/>
             </div>
-        </div>
-        <Outlet/>
-      </Fragment>
+        </Fragment>
     )
-  }
+    }
+    else{
+        return(
+          <Fragment>
+            <div className="navigation">
+                <Link className="logo-container" to="/">
+                    <Logo className="logo"/>
+                </Link>
+                <div className="nav-links-container">
+                    <Link className="nav-link" to='/shop'>
+                        About us
+                    </Link>
+                    <Link className="nav-link" to='/shop'>
+                        Forum
+                    </Link>
+                    <Link className="nav-link" to='/shop'>
+                        Contact
+                    </Link>
+                    <Link className="nav-link" to='/shop'>
+                        Sign In
+                    </Link>
+                </div>
+            </div>
+            <Outlet/>
+          </Fragment>
+        )  
+}
+    }
+    
+  
 
 export default Navigation
