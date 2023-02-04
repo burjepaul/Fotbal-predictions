533f0bcb0e0cfc1d15c858a59d34034d602057be
burjepaul <burjepaul97@gmail.com>
2023-02-04 10:14:29 +0200

    Fixed yesterday results

--- src/routes/yesterdayResults/yesterdayResults.component.jsx
+++ src/routes/yesterdayResults/yesterdayResults.component.jsx
@@ -5,15 +5,23 @@ import Spinner from '../../components/spinner/spinner'
 import './yesterdayResults.styles.scss'
 
 const YesterdayResults = () => {
+    let date = new Date()
+
+    const day = date.getDate()-1
+    const month = date.getMonth()+1
+    const year = date.getFullYear()
+    const build_date = year + "-" + month + "-" + day
+
+
     const [toadyMatches, setTodayMatches] = useState([])
     const [isLoadingData, setIsLoadingData] = useState(false)
     const [error, setError] = useState(null)
-
+    
     const fetchTodayMatchesHandler = useCallback(async () => {
         setIsLoadingData(true)
         setError(null)
         try{
-            const response = await fetch('https://fotbal.herokuapp.com/matches/yesterdayResults/')
+            const response = await fetch(`https://fotbal.herokuapp.com/matches/yesterdayResults/?date=${build_date}`)
             if (!response.ok){
             throw new Error('Something went wrong!')
             }
@@ -25,12 +33,12 @@ const YesterdayResults = () => {
             setError(error.message)
         }
         setIsLoadingData(false)
-    }, []
+    }, [build_date]
     )
 
     useEffect(() => {
         fetchTodayMatchesHandler()
-    },[fetchTodayMatchesHandler])
+    },[fetchTodayMatchesHandler, build_date])
 
     let content;
 
