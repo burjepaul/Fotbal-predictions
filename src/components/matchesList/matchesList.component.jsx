5973a0493f2b00029aed5334dec26b91e01b18e9
burjepaul <burjepaul97@gmail.com>
2023-02-04 21:50:17 +0200

    Sort by component

--- src/components/matchesList/matchesList.component.jsx
+++ src/components/matchesList/matchesList.component.jsx
@@ -1,9 +1,35 @@
 import Match from '../match/match.component';
+import {ReactComponent as DropDownArrow} from '../../assets/dropdown-arrow.svg'
 import './matchesList.styles.scss'
+import { useState } from 'react';
 
 const MatchesList = (props) => {
+  const [dropDown, setDropDown] = useState(false)
+  const [dropDownClass, setDropDownClass] = useState("drop-down-menu hide")
+
+  const handleDropDown = () => {
+    if (!dropDown){
+      setDropDown(!dropDown)
+      setDropDownClass("drop-down-menu visible")
+    }else{
+      setDropDown(!dropDown)
+      setDropDownClass("drop-down-menu hide")
+    }
+  }
+
+
   return (
     <div className='matches-list'>
+      <div className='sort-component'>
+        <h2>Sort by :&nbsp;&nbsp;
+          <button onClick={handleDropDown}><DropDownArrow/> Playing hour</button>
+        <div className={dropDownClass}>
+          <div class='drop-down-item'>Win Chance</div>
+          <div class='drop-down-item'>Odd</div>
+          <div class='drop-down-item'>Country</div>
+        </div>
+        </h2>
+      </div>
       {props.matches.map((match) => (
         <Match
           key={match.id}
