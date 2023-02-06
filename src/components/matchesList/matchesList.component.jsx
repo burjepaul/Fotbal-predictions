2d86c9ef6a45104a635c55979cd30dcab37ab886
burjepaul <burjepaul97@gmail.com>
2023-02-06 22:13:29 +0200

    Sort functionality

--- src/components/matchesList/matchesList.component.jsx
+++ src/components/matchesList/matchesList.component.jsx
@@ -10,6 +10,7 @@ const MatchesList = (props) => {
   const [dropDown, setDropDown] = useState(false)
   const [dropDownClass, setDropDownClass] = useState("drop-down-menu hide")
   const [activeDropDown, setActiveDropDown] = useState(dropDownText)
+  const [activeDropDownItem, setActiveDropDownItem] = useState(dropDownText[0])
 
   const handleDropDown = () => {
     if (!dropDown){
@@ -28,6 +29,7 @@ const MatchesList = (props) => {
     setActiveDropDown(activeDropDown)
     setDropDown(!dropDown)
     setDropDownClass("drop-down-menu hide")
+    setActiveDropDownItem(activeDropDown[0])
   }
   const handleDropDownItem2 = () => {
     const temp = activeDropDown[0]
@@ -36,6 +38,7 @@ const MatchesList = (props) => {
     setActiveDropDown(activeDropDown)
     setDropDown(!dropDown)
     setDropDownClass("drop-down-menu hide")
+    setActiveDropDownItem(activeDropDown[0])
   }
   const handleDropDownItem3 = () => {
     const temp = activeDropDown[0]
@@ -44,7 +47,24 @@ const MatchesList = (props) => {
     setActiveDropDown(activeDropDown)
     setDropDown(!dropDown)
     setDropDownClass("drop-down-menu hide")
+    setActiveDropDownItem(activeDropDown[0])
   }
+
+  switch(activeDropDownItem){
+    case "Odd":
+      props.matches.sort((a,b) => (a.odd > b.odd) ? 1 : ((b.odd > a.odd) ? -1 : 0));
+      break;
+    case "Win Chance":
+      props.matches.sort((a,b) => b.score_difference - a.score_difference);
+      break;
+    case "Country":
+      props.matches.sort((a,b) => (a.country > b.country) ? 1 : ((b.country > a.country) ? -1 : 0));
+      break;
+    default:
+      props.matches.sort((a,b) => (a.playing_hour > b.playing_hour) ? 1 : ((b.playing_hour > a.playing_hour) ? -1 : 0));
+
+  }
+
 
 
   return (
