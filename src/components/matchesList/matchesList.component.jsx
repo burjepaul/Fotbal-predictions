e120f324e41ba371c07a24f73db775f00ae63a70
burjepaul <burjepaul97@gmail.com>
2023-02-09 20:03:07 +0200

    Pagination Functionality

--- src/components/matchesList/matchesList.component.jsx
+++ src/components/matchesList/matchesList.component.jsx
@@ -1,85 +1,56 @@
 import Match from '../match/match.component';
-import {ReactComponent as DropDownArrow} from '../../assets/dropdown-arrow.svg'
+import {ReactComponent as NextPageArrow} from '../../assets/pagearrow-right.svg'
+import {ReactComponent as PreviousPageArrow} from '../../assets/pagearrow-left.svg'
 import './matchesList.styles.scss'
 import { useState } from 'react';
+import SortBy from '../sortBy/sortby.component';
 
 const MatchesList = (props) => {
+  const [, setActiveDropDownItem] = useState("Playing hour")
+  const [activePage, setActivePage] = useState(1)
 
-  const dropDownText = ["Playing hour", "Win Chance", "Odd", "Country"]
+  const handleActiveDropDownItem = (newMatches) => {
+     setActiveDropDownItem(newMatches)
+     switch(newMatches){
+       case "Odd":
+         props.matches.sort((a,b) => (a.odd > b.odd) ? 1 : ((b.odd > a.odd) ? -1 : 0));
+         break;
+       case "Win Chance":
+         props.matches.sort((a,b) => b.score_difference - a.score_difference);
+           break;
+       case "Country":
+         props.matches.sort((a,b) => (a.country > b.country) ? 1 : ((b.country > a.country) ? -1 : 0));
+         break;
+       default:
+         props.matches.sort((a,b) => (a.playing_hour > b.playing_hour) ? 1 : ((b.playing_hour > a.playing_hour) ? -1 : 0));
+       }
+      }
 
-  const [dropDown, setDropDown] = useState(false)
-  const [dropDownClass, setDropDownClass] = useState("drop-down-menu hide")
-  const [activeDropDown, setActiveDropDown] = useState(dropDownText)
-  const [activeDropDownItem, setActiveDropDownItem] = useState(dropDownText[0])
+  let totalPages = Math.ceil(props.matches.length/10)
 
-  const handleDropDown = () => {
-    if (!dropDown){
-      setDropDown(!dropDown)
-      setDropDownClass("drop-down-menu visible")
-    }else{
-      setDropDown(!dropDown)
-      setDropDownClass("drop-down-menu hide")
+  const handlePages = (incerement) => {
+    if ((activePage+incerement >= 1) && (activePage+incerement <= totalPages)){
+      setActivePage(activePage+incerement)
     }
   }
 
-  const handleDropDownItem1 = () => {
-    const temp = activeDropDown[0]
-    activeDropDown[0] = activeDropDown[1]
-    activeDropDown[1] = temp
-    setActiveDropDown(activeDropDown)
-    setDropDown(!dropDown)
-    setDropDownClass("drop-down-menu hide")
-    setActiveDropDownItem(activeDropDown[0])
-  }
-  const handleDropDownItem2 = () => {
-    const temp = activeDropDown[0]
-    activeDropDown[0] = activeDropDown[2]
-    activeDropDown[2] = temp
-    setActiveDropDown(activeDropDown)
-    setDropDown(!dropDown)
-    setDropDownClass("drop-down-menu hide")
-    setActiveDropDownItem(activeDropDown[0])
-  }
-  const handleDropDownItem3 = () => {
-    const temp = activeDropDown[0]
-    activeDropDown[0] = activeDropDown[3]
-    activeDropDown[3] = temp
-    setActiveDropDown(activeDropDown)
-    setDropDown(!dropDown)
-    setDropDownClass("drop-down-menu hide")
-    setActiveDropDownItem(activeDropDown[0])
-  }
-
-  switch(activeDropDownItem){
-    case "Odd":
-      props.matches.sort((a,b) => (a.odd > b.odd) ? 1 : ((b.odd > a.odd) ? -1 : 0));
-      break;
-    case "Win Chance":
-      props.matches.sort((a,b) => b.score_difference - a.score_difference);
-      break;
-    case "Country":
-      props.matches.sort((a,b) => (a.country > b.country) ? 1 : ((b.country > a.country) ? -1 : 0));
-      break;
-    default:
-      props.matches.sort((a,b) => (a.playing_hour > b.playing_hour) ? 1 : ((b.playing_hour > a.playing_hour) ? -1 : 0));
-
-  }
-
-
-
   return (
     <div className='matches-list'>
-      <div className='sort-component'>
-        <h2>Sort by :&nbsp;&nbsp;
-          <button onClick={handleDropDown}><DropDownArrow/>&nbsp;&nbsp;{activeDropDown[0]}</button>
-        <div className={dropDownClass}>
-          <div className='drop-down-item' onClick={handleDropDownItem1}>{activeDropDown[1]}</div>
-          <div className='drop-down-item' onClick={handleDropDownItem2}>{activeDropDown[2]}</div>
-          <div className='drop-down-item' onClick={handleDropDownItem3}>{activeDropDown[3]}</div>
-        </div>
-        </h2>
+      <div className='mathclist-header'>
+            <div className="title"></div>
+            <div className='pagination'>
+                <div className='arrows' onClick={() => handlePages(-1)}>
+                    <PreviousPageArrow/>
+                </div>
+                <h2 className="title">Page&nbsp;&nbsp;</h2>
+                <h2>{activePage}/{totalPages}</h2>
+                <div className='arrows' onClick={() => handlePages(+1)}>
+                  <NextPageArrow/>
+                </div>
+            </div>
+        <SortBy matches={props.matches} handleActiveDropDownItem={handleActiveDropDownItem}/>
       </div>
-      {props.matches.map((match) => (
+      {props.matches.slice(activePage * 10 - 10, activePage * 10 - 1).map((match) => (
         <Match
           key={match.id}
           playing_date={match.playing_date}
---
+++ src/components/sortBy/sortby.component.jsx
@@ -0,0 +1,63 @@
+import { useState } from 'react';
+import {ReactComponent as DropDownArrow} from '../../assets/dropdown-arrow.svg'
+
+const SortBy = ({matches, handleActiveDropDownItem}) => {
+    const dropDownText = ["Playing hour", "Win Chance", "Odd", "Country"]
+
+    const [dropDown, setDropDown] = useState(false)
+    const [dropDownClass, setDropDownClass] = useState("drop-down-menu hide")
+    const [activeDropDown, setActiveDropDown] = useState(dropDownText)
+      
+    const handleDropDown = () => {
+        if (!dropDown){
+          setDropDown(!dropDown)
+          setDropDownClass("drop-down-menu visible")
+        }else{
+          setDropDown(!dropDown)
+          setDropDownClass("drop-down-menu hide")
+        }
+      }
+    
+      const handleDropDownItem1 = () => {
+        const temp = activeDropDown[0]
+        activeDropDown[0] = activeDropDown[1]
+        activeDropDown[1] = temp
+        setActiveDropDown(activeDropDown)
+        setDropDown(!dropDown)
+        setDropDownClass("drop-down-menu hide")
+        handleActiveDropDownItem(activeDropDown[0])
+      }
+      const handleDropDownItem2 = () => {
+        const temp = activeDropDown[0]
+        activeDropDown[0] = activeDropDown[2]
+        activeDropDown[2] = temp
+        setActiveDropDown(activeDropDown)
+        setDropDown(!dropDown)
+        setDropDownClass("drop-down-menu hide")
+        handleActiveDropDownItem(activeDropDown[0])
+      }
+      const handleDropDownItem3 = () => {
+        const temp = activeDropDown[0]
+        activeDropDown[0] = activeDropDown[3]
+        activeDropDown[3] = temp
+        setActiveDropDown(activeDropDown)
+        setDropDown(!dropDown)
+        setDropDownClass("drop-down-menu hide")
+        handleActiveDropDownItem(activeDropDown[0])
+      }
+
+    return (
+    <div className='sort-component'>
+        <h2>Sort by :&nbsp;&nbsp;
+              <button onClick={handleDropDown}><DropDownArrow/>&nbsp;&nbsp;{activeDropDown[0]}</button>
+          <div className={dropDownClass}>
+              <div className='drop-down-item' onClick={handleDropDownItem1}>{activeDropDown[1]}</div>
+              <div className='drop-down-item' onClick={handleDropDownItem2}>{activeDropDown[2]}</div>
+              <div className='drop-down-item' onClick={handleDropDownItem3}>{activeDropDown[3]}</div>
+          </div>
+        </h2>
+    </div>
+    )
+}
+
+export default SortBy
