31928f1cb42c016c309db27c0e821065ac177408
burjepaul <burjepaul97@gmail.com>
2023-02-23 18:49:07 +0200

    Abot ass, first commit

--- src/routes/aboutus/aboutus.component.jsx
+++ src/routes/aboutus/aboutus.component.jsx
@@ -10,7 +10,7 @@ const Aboutus = () => {
             <div className='image-text'>
                 <p>We are trying to predict the fotbal matches results using an automate soft. The automation is done by scanning all the fotbal matches that are going to be 
                     played tomorrow, and base on the teams form it is going to predict the winner (see below). Each day there will be at least 10 games that are most likely to be correct,
-                    depending of overall loading. The scanning is done only on competition where there is a standing for cups, frendly matches and other such events being ignored.</p>
+                    depending of overall loading. The scanning is done only on competition where there is a standing, for cups, frendly matches and other such events being ignored.</p>
             </div>  
         </div>
         </>
