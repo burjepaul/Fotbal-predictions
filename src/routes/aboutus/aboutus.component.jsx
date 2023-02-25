2e503f802b8c577e150ef4118cec9bcbd8e61854
burjepaul <burjepaul97@gmail.com>
2023-02-25 14:02:59 +0200

    About us, exeple 2

--- src/routes/aboutus/aboutus.component.jsx
+++ src/routes/aboutus/aboutus.component.jsx
@@ -1,6 +1,7 @@
 import './aboutus.styles.scss'
 import img from '../../assets/aboutusImage.jpg'
 import firstExemple from '../../assets/firstExemple.png'
+import secondExemple from '../../assets/secondExemple.png'
 
 const Aboutus = () => {
     return (
@@ -18,10 +19,19 @@ const Aboutus = () => {
             <h2>How the computation is made:</h2>
             <div className='first-exemple'>
                 <h2>1)</h2>
-                <p>For this first example we have Hoffenheim playng agains Dortmund. Below we can se that Dortmund comes after 5 wins in a row, so it is in a good form, on the other hand,
+                <p> For this first example we have Hoffenheim playng agains Dortmund. Below we can se that Dortmund comes after 5 wins in a row, so it is in a good form, on the other hand,
                     we have Hoffenheim with no win, just one equal and 4 loses. In this case the output will come as a victory for Dotmund.
                 </p>
                 <img src={firstExemple} alt="first exemple" className='first-exemple-img'/>
+            </div>
+            <div className='first-exemple'>
+                <h2>2)</h2>
+                <p> In this second exemple the form difference between Sheffield and Charlton is not so obvious, but in this case it's taken into account the teams aganst they have
+                    played. Sheffield's poorest result (1 draw) being against the 3rd team of the standing, while Charlton best to results (2 wins) are agains the last team and one
+                    from the second part of the standing. By calculating also the wins (for Sheffield) and the losses (for Charlton) against the teams who has been made, a total score
+                    difference is establish. The higher the score difference is, the higher the chances of win are (you can sort every day predictions by this value).
+                </p>
+                <img src={secondExemple} alt="first exemple" className='first-exemple-img'/>
             </div>
         </div>
         </>
