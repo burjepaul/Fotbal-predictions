11d8219e74c3ae1f54914a0349ab333f3bc8a2cc
burjepaul <burjepaul97@gmail.com>
2023-06-23 11:30:47 +0300

    After statistics DB setup

--- src/routes/statistics/statistics.component.jsx
+++ src/routes/statistics/statistics.component.jsx
@@ -1,6 +1,6 @@
 import { Fragment, useContext } from 'react'
 import ByCountry from './byCountry/byCountry.component'
-import ByOdd from './byOdd/byOdd.component'
+// import ByOdd from './byOdd/byOdd.component'
 import './statistics.styles.scss'
 import Tops from './tops/tops.component'
 import { StatisticsContext } from '../../contexts/statistics.context'
@@ -17,7 +17,7 @@ const Statistics = () => {
         <Fragment>
             <h1>Statistics</h1>
             <div className='statistics-container'>
-                <ByOdd className='byOddItem'/>
+                {/* <ByOdd className='byOddItem'/> */}
                 {output}
                 <div className='topsItem'>
                     <Tops/>
