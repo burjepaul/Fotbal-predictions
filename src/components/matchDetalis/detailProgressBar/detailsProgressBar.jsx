bbc0b6024c3ac68a2cd0441313bcbb9cc3182276
burjepaul <burjepaul97@gmail.com>
2023-07-05 22:56:54 +0300

    After statistics entry

--- src/components/matchDetalis/detailProgressBar/detailsProgressBar.jsx
+++ src/components/matchDetalis/detailProgressBar/detailsProgressBar.jsx
@@ -14,7 +14,7 @@ const DetailProgressBar = ({percent}) => {
             borderRadius='5px'
             className='detail-progrssbar'
             width='100%'
-            height='2vh'
+            height='3.5vh'
             bgColor={`hsl(${hslValue}, 100%, 50%)`}
             baseBgColor="rgb(136, 16, 16)"
             labelClassName="label"
