0f30cc5b502d60091193bb9336e42b4bceb5d611
burjepaul <burjepaul97@gmail.com>
2023-08-28 15:03:22 +0300

    Terms of use page

--- src/routes/FooterPages/TermosOfUse/termosOfUse.jsx
+++ src/routes/FooterPages/TermosOfUse/termosOfUse.jsx
@@ -4,7 +4,70 @@ import "./termsOfUse.styles.scss"
 const TermsOfUse = () => {
     return(
         <>
-            <h1>Terms Of Use</h1>
+            <div className='terms-container'>
+                <h2>
+                    Terms of Use
+                </h2>
+                    <p className='subTitle'>Introductory information</p>
+                    <p>These terms of use govern the mutual rights and obligations between TheOracle.com, (“we”, “us” or “our”) and third party individuals (“User”, “you” or “your”) when using our website.</p>
+                    <p className='subTitle'><b>I.</b> Site Content</p>
+                    <p className='subsubTitle'><b>I.I</b> Nature of the service.</p>
+                    <p>The Site contains up-to-date information on fotball events, in particular predictions on sporting results, final results, and goals scored. Results and other statistical information displayed on the Site are based on information provided by other independent sources (from third parties), internal efforts or other official applications. Whilst we make every effort to regularly update the content and check the results or other information displayed on the Site, we do not make any promises or grant any warranties about the Site, and we encourage you to thoroughly check the information collected on the Site with original and other sources as well. The use of and reliance on the results and other information displayed on the Site is your sole responsibility. Depending on the region, language or other circumstances affecting the target market and audience of the Site, the Site may also feature sports coverage consisting of our content or content from third parties.</p>
+                    <p className='subsubTitle'><b>I.II</b> Use of the Site at your own risk.</p>
+                    <p>We are providing the Site, and all the communication and information stored and presented therein, with reasonable skill and care, and substantially in the manner described in these Terms of Use. However, your access to the Site, use of the Site, downloading of any software relating to the Site, and use of any information we may provide in connection with the Site, is at your sole option, discretion, risk, and for your personal use only. You may not use the Site for any commercial purpose.</p>
+                    <p className='subsubTitle'><b>I.III</b> Display of advertising.</p>
+                    <p>You agree that advertising, including third party advertising, may be displayed on the service.</p>
+                    <p className='subsubTitle'><b>I.IV</b> Relationship to gambling.</p>
+                    <p>Use of the Site is entirely at your own risk. The Site is not a gaming or gambling application. We do not provide games or gambling; therefore, we do not hold or control your financial or other resources and do not participate in any gambling transactions. The betting odds displayed on the Site are presented for news purposes. No communication or information published on the Site constitutes a recommendation to participate in a game or to place a bet, nor does it constitute legal, tax or other similar advice in connection with gaming or gambling.</p>
+                    <p className='subsubTitle'><b>I.V</b> Local law.</p>
+                    <p>You are advised to comply with the applicable laws of the country in which you are temporarily or permanently resident, present and/or a citizen of.</p>
+                    <p className='subsubTitle'><b>I.VI</b> Unauthorized interference.</p>
+                    <p>You must not use any mechanism, tool, software or procedure that has or could adversely affect the operation of our facilities, the security of the Internet or other Internet users. You may not burden our server on which the Site is hosted with automated requests, nor may you assist any third party in such activity. You may not modify, disassemble, decompile or reverse engineer the website in any way, unless otherwise provided for by generally binding legal regulations. Furthermore, you are not permitted to use the content of the website by embedding, aggregating, scraping or recreating it without our express consent, unless otherwise provided for by generally binding legal regulations.</p>
+                   
+                    <p className='subTitle'><b>II.</b> User Obligations</p>
+                    <p className='subsubTitle'><b>II.I</b> Lawful Use.</p>
+                    <p> You agree to use the Website in strict compliance with all applicable laws and regulations. You shall not engage in any activities that are unlawful, fraudulent, or violate the rights of others.</p>
+                    <p className='subsubTitle'><b>II.II</b> Respectful Conduct.</p>
+                    <p> You shall conduct yourself in a respectful and considerate manner while using the Website. This includes refraining from engaging in any form of harassment, hate speech, or offensive behavior towards other users.</p>
+                    <p className='subsubTitle'><b>II.III</b> Accuracy and Integrity.</p>
+                    <p> When interacting with the Website's content, including football predictions and discussions, you shall provide accurate and honest information. Do not intentionally spread false or misleading information.</p>
+                    <p className='subsubTitle'><b>II.IV</b> No Unlawful Content.</p>
+                    <p> You shall not upload, share, or distribute any content that is defamatory, obscene, libelous, or otherwise harmful. This includes refraining from posting any content that infringes upon the intellectual property rights of others.</p>
+                    <p className='subsubTitle'><b>II.V</b> No Malicious Activities.</p>
+                    <p> You shall not engage in any activities that could harm the Website's functionality, security, or availability. This includes, but is not limited to, attempting to gain unauthorized access to the Website or engaging in any form of hacking or cyberattacks.</p>
+                    <p className='subsubTitle'><b>II.VI</b> Respect Copyright and Intellectual Property.</p>
+                    <p> You shall respect the copyright and intellectual property rights of the Website and other users. Do not use, copy, or distribute any content from the Website without proper authorization.</p>
+                    <p className='subsubTitle'><b>II.VII</b> No Commercial Use.</p>
+                    <p> You shall not use the Website for commercial purposes, including advertising, promotions, or solicitation, without obtaining explicit permission from the Website administrators.</p>
+                    <p className='subsubTitle'><b>II.VIII</b> Age Restriction.</p>
+                    <p> If you are under the legal age to consent to the terms of this agreement in your jurisdiction, you must obtain the consent of a parent or legal guardian before using the Website.</p>
+                    <p className='subsubTitle'><b>II.IX</b> No Impersonation.</p>
+                    <p> You shall not impersonate any individual or entity, including other users, administrators, or third parties.</p>
+                    <p className='subsubTitle'><b>II.X</b> Compliance with Community Guidelines.</p>
+                    <p> You agree to follow any community guidelines or rules posted by the Website administrators. These guidelines are designed to ensure a positive and respectful community environment.</p>
+
+                    <p className='subTitle'><b>III. </b> Account Creation (Coming Soon)</p>
+                    <p className='subsubTitle'><b>III.I</b> Stay Updated.</p>
+                    <p> Keep an eye on our announcements and updates for the official launch of the account creation feature. We will notify you via our website and any other relevant channels once the feature becomes available.</p>
+                    <p className='subsubTitle'><b>III.II</b> Personalized Experience.</p>
+                    <p> With an account, you will have the opportunity to personalize your interaction with the website. This may include saving your preferences, tracking your predictions, and participating in community discussions.</p>
+                    <p className='subsubTitle'><b>III.III</b> Enhanced Prediction Tracking</p>
+                    <p> An account will allow you to track your football predictions over time. You'll be able to monitor your success rate, review past predictions, and analyze your performance.</p>
+                    <p className='subsubTitle'><b>III.IV</b> Community Participation.</p>
+                    <p> Engage more deeply with other users by joining discussions, sharing insights, and contributing to the community's knowledge base.</p>
+                    <p className='subsubTitle'><b>III.V</b> Exclusive Content.</p>
+                    <p>In the future, registered users may gain access to exclusive content, such as additional prediction insights, expert analysis, and special offers. or cyberattacks.</p>
+                    <p className='subsubTitle'><b>III.VI</b> Efficient Support.</p>
+                    <p> Having an account will make it easier for us to provide you with personalized support if you encounter any issues or have questions.</p>
+                    <p className='subsubTitle'><b>III.VII</b> Account Security.</p>
+                    <p> Rest assured that we are committed to implementing robust security measures to protect your account information and ensure your privacy.</p>
+                    <p className='subsubTitle'><b>III.VIII</b> No Obligation.</p>
+                    <p> While account creation will be an option, it will not be mandatory. You can continue to access our basic prediction content without an account.</p>
+
+                    <p className='subTitle'><b>IV. </b> Cookies</p>
+                    <p className='subsubTitle'><b>IV.I</b> Cookie consent tool.</p>
+                    <p> Obtaining your consent and providing you with information related to the use of cookies within the Site are fulfilled through a special tool operated by a third party.</p>
+            </div>
         </>
     )
 }
