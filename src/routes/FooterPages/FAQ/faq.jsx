6ce04f71d3f8ac468ff698fa72a4d8c479d73a03
burjepaul <burjepaul97@gmail.com>
2023-08-29 18:36:46 +0300

    FAQ page

--- src/routes/FooterPages/FAQ/faq.jsx
+++ src/routes/FooterPages/FAQ/faq.jsx
@@ -8,12 +8,14 @@ const FAQ = () => {
         'ARE THE PREDICTIONS FILTERED BY CONSIDERING NON-STATISTICAL ELEMENTS?',
         'IN WHICH PART OF THE DAY ARE THE PREDICTIONS PUBLISHED?',
         'WHAT TYPE OF PREDICTIONS DO YOU PROVIDE?',
+        'WHY THERE ARE NO UEFA CHAMPIONS LAEGUE/UEFA EUROPA LEAGUE/UEFA EUROPA CONFERENCE LEAGUE PREDICTIONS?'
     ]
     const answers = [
         'Yes, for every standings, mathematical calculations are being done based on temas form on away or home field, on average goals scored. For more details see About Us section Way of calculating.',
-        'The predictions are 100% generated, non-statistical elements, like lack of confidence or playng with the 2nd team, are not taken into account.',
-        'Usually, we are trying to run our software at 00:00 AM UTC+00, but we may encounter encounter difficultys and it can be delayed until 08:00 AM. Here comes into play the porpouse of "Tommorw Predictions" in case you are in a hurry and the "Today Matches" has not been updated.',
-        'For now, there are only 2 types of predictions, the winning team (can be draw) and the expected goals to be scored.'
+        'The predictions are 100% generated. Non-statistical elements, like lack of confidence or playng with the 2nd team, are not taken into account.',
+        'Usually, we are trying to run our software at 00:00 AM UTC+00, but we may encounter encounter difficultys and it can be delayed until 08:00 AM. Here comes into play the porpouse of "Tommorw Predictions" in case you are in a hurry and the "Today Matches" has not been updated yet.',
+        'For now, there are only 2 types of predictions, the winning team (can be draw) and the expected goals to be scored.',
+        'For now, the software is programmed to do analyses only over mathces that are part of a standing. Matches from elimination competitions, matches of nationa teams, frendly games are being ignored.'
     ]
 
     return(
